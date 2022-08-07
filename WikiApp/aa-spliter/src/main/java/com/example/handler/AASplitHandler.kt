65007c5dc7d8b04c675ebcf7c2ea2e811b833f34
Ryan Chen <lemon@Ryans-MacBook-Air.local>
2022-08-07 10:25:50 -0400

    Delegation

--- WikiApp/aa-spliter/src/main/java/com/example/handler/AASplitHandler.kt
+++
@@ -1,9 +0,0 @@
-package com.example.handler
-import com.example.model.UserInfo
-
-interface AASplitHandler {
-    val userInfoList: List<UserInfo>
-    fun onAddPersonClicked(userInfo: UserInfo)
-    fun onCalculateClicked()
-    fun onClearClicked()
-}
--- WikiApp/aa-spliter/src/main/java/com/example/model/UserInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.model
-
-data class UserInfo(
-    val name: String,
-    val spending: String,
-    var totalSpend: Float,
-    val balance: String
-)
